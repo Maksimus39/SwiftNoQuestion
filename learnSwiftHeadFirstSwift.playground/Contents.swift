a0fe4bf047504090fca98ba0836e33c53728d1e9
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-02 11:59:54 -0700

    знакомство с типами итд

--- learnSwiftHeadFirstSwift.playground/Contents.swift
+++ learnSwiftHeadFirstSwift.playground/Contents.swift
@@ -17,3 +17,86 @@
 //
 //let myPizza = Pizza()
 //myPizza.showName()
+
+
+// ---------------------------------------------------------------------------------------------------------
+
+// Pizza
+
+//var ingridients = [
+//    "Pepperoni",
+//    "Mozzarella",
+//    "Basil",
+//    "Garlic",
+//    "Onion",
+//    "Oregano",
+//    "Mushroom",
+//    "Tomato",
+//    "Red Pepper",
+//    "Ham",
+//    "Chicken",
+//    "Red Onion",
+//    "Black Olives",
+//    "Bell Pepper",
+//    "Pineapple",
+//    "Canadian Bacon",
+//    "Salami",
+//    "Jalapeno",
+//    "Spinach",
+//    "Italian Sausage",
+//    "Provolone",
+//    "Pesto",
+//    "Sun - Dried Tomatoes",
+//    "Feta",
+//    "Meatballs",
+//    "Prosciutto",
+//    "Cherry Tomatoes",
+//    "Pulled Pork",
+//    "Chorizo",
+//    "Anchovy",
+//    "Capers"
+//]
+
+//print(ingridients.randomElement()!)
+//print(ingridients.last!)
+//print(ingridients.append("Banana"))
+//print(ingridients)
+
+//print("\(ingridients.randomElement()!), \(ingridients.randomElement()!), \(ingridients.randomElement()!), \(ingridients.randomElement()!)")
+
+
+//let num:Int = 5
+//let num2:Int = 42
+//
+//let res = num * num2
+//print(res)
+//
+//let dev = (res / 3) - 4
+//
+//print(dev)
+
+//let res = 4 + 5 * 5
+//print(res)
+
+//var pizzaSlicesremaining = 8
+//let totalSlices = 8
+//print(totalSlices / pizzaSlicesremaining)
+//pizzaSlicesremaining = 4
+//print(totalSlices / pizzaSlicesremaining)
+
+//var integer = 2
+//integer = "Max"
+
+
+//var pizzaShowName = "Big Mike`s Pizza"
+//pizzaShowName = "Swift Pizza"
+//print(pizzaShowName)
+
+
+//let name:String
+//name = "Max"
+//print(name)
+//
+//
+//let nameTwo = "Larisa"
+//print(nameTwo)
