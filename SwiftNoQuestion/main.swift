fbccca45712ec13fea60a660f97269b0106de2ff
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-01 22:54:43 -0700

    day  2

--- SwiftNoQuestion/main.swift
+++ SwiftNoQuestion/main.swift
@@ -43,3 +43,115 @@
 //let firstName = readLine()!
 //print("Привет, \(name) \(firstName)!")
 
+
+
+// ----------------------------------------------- Преобразование типов -------------------------------------------------------------
+
+
+
+//var a = 10
+//var b = 2.5
+//var c  = Double(a) + b
+//var z = a + Int(b)
+//print(c, z)
+
+
+//var num = 10
+//var str = String(num)
+//print(type(of: str))
+
+
+//print("Введите ваш возраст?")
+//var name = readLine()!
+//print("Ваш возраст \(name) лет")
+
+
+
+// ----------------------------------------------- Математические  операции  -------------------------------------------------------------
+
+
+//var a = 10
+//var b = 20
+//var c = a + b
+//print(c)
+//
+//
+//var num:Int
+//num = 10
+//
+//var numTwo:Double
+//numTwo = 30.7
+//
+//
+//let result = Double(num) + numTwo
+//print(result)
+//print(type(of: result))
+
+//var a:Int
+//a = 10
+//a = a + 1
+//a += 1
+//print(a)
+
+
+// Решение задачек
+
+// 1
+
+//print("Введите два числа")
+//var firstNumber = Int(readLine()!)!
+//var secondNumber = Int(readLine()!)!
+//
+//let result = firstNumber + secondNumber
+//print(result)
+
+
+// 2
+
+//print("Введите три числа")
+//var firstNumber = Double(readLine()!)!
+//var secondNumber = Double(readLine()!)!
+//var thirdNumber = Double(readLine()!)!
+//
+//let result = (firstNumber + secondNumber) / thirdNumber
+//print(result)
+
+
+// 3
+
+//print("Сколько друзей было с вами в баре?")
+//let friends = Int(readLine()!)!
+//print("На какую сумму вышел счёт?")
+//let sum = Double(readLine()!)!
+//print("Значит с каждого будет по \(sum / Double(friends)) рублей!")
+
+
+
+// ----------------------------------------------- Сложение  строк  -------------------------------------------------------------
+
+//let name = "Василий"
+//let surname = "Петров"
+//let fullName = name + " " + surname
+//print(fullName)
+
+// 1
+
+//print("Введите ваше имя?")
+//var name = readLine()!
+//print("Введите вашу фамилию?")
+//var surname = readLine()!
+//let fullName = name + " " + surname
+//print("Ваше полное имя: \(fullName)")
+
+
+// 2
+
+//print("Введите ваше имя?")
+//var name = readLine()!
+//
+//print("Какой ваш вес?")
+//var weight = Double(readLine()!)!
+//
+//print("Ваш полное имя: \(name), ваш вес: \(weight) кг")
+
+
