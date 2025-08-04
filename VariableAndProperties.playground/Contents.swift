870282ada450b106bb49cbea2bd247ec07896b01
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-03 21:25:18 -0700

    Наблюдатели свойств

--- VariableAndProperties.playground/Contents.swift
+++ VariableAndProperties.playground/Contents.swift
@@ -1,1 +1,34 @@
 // --------------------- Переменные и свойства -------------------------------------------
+
+
+//var num:Int = 10
+//num = 20
+//print(num)
+//
+//
+//let str:String = "Hello, world!"
+
+
+
+// Наблюдатели свойств
+
+//var myProperty = 5 {
+//    willSet {
+//       print("Will set to \(newValue) . It was previously \(myProperty)")
+//    }
+//    didSet {
+//        print("Did set to \(myProperty) . It was previously \(oldValue)")
+//    }
+//}
+//
+//myProperty = 6
+
+
+var myFontSize = 10 {
+    willSet(newFontSize){
+        print("Will set font to \(newFontSize), it was \(myFontSize)")
+    }
+    didSet(oldFontSize){
+        print("Did set font to \(myFontSize), it was \(oldFontSize)")
+    }
+}
