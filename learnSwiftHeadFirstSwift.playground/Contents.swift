17a2f19f4cb1191bcd0903836d7150f0ed4857b4
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-18 10:50:41 -0700

    знакомство с языком, типы данных

--- learnSwiftHeadFirstSwift.playground/Contents.swift
+++ learnSwiftHeadFirstSwift.playground/Contents.swift
@@ -1,1 +1,44 @@
+// Знакомство с языком, типы данных, константы и переменные.
 
+
+// Basic
+
+var name = "Maksim"
+print(name)
+name = "Larisa"
+print(name)
+
+var name2 : String = "Maksim"
+print(name2)
+name2 = "Larisa"
+
+
+var nameMaksim = "Maksim", nameLarisa = "Larisa"
+print("nameLarisa:", nameLarisa)
+
+var nameSon : String
+nameSon = "Andrey"
+print(nameSon)
+
+
+var double : Double = 5.5
+print(double)
+
+var flt : Float = 0.12345
+print(flt)
+
+
+var bl : Bool = true
+print(bl)
+
+bl = false
+print("bl:", bl)
+
+
+var age : Int8 = 127
+print(age)
+
+
+let nameIvan : String = "Ivan"
+
+let namePetr : String = "Petr"
