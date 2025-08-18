7f83da0b505904f523059d75045677f3b4a894e6
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-18 14:45:49 -0700

    input data

--- SwiftNoQuestion/main.swift
+++ SwiftNoQuestion/main.swift
@@ -1,1 +1,35 @@
+// ------------------------------------------- BASIC -----------------------------------------------------------------
 
+// Ввод информации
+
+//print("Введите ваше имя:")
+//var name = readLine()
+//print(name!)
+
+
+// task 1
+
+//print("Введите ваше имя:")
+//var name : String = readLine()!
+//print("Привет, \(name)!")
+
+
+// task 2
+
+//print("Введите ваше имя и фамилию:")
+//print("Имя?:")
+//var name : String = readLine()!
+//print("Фамилия?:")
+//var surname : String = readLine()!
+//print("Привет, \(name) \(surname)!")
+
+
+// task 3
+
+//let line1 = "Ели мясо мужики"
+//let line2 = "пивом запивали"
+//let line3 = "О чём конюх говорил"
+//let line4 = "они не понимали"
+//
+//let resLine = line1 + ", " + line2 + "! " + line3 + ", " + line4 + "!"
+//print(resLine)
