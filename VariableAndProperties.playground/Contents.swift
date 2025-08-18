88e4502030be02885d1933b0795ee621aa413a4c
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-18 15:14:57 -0700

    learn Class and properties

--- VariableAndProperties.playground/Contents.swift
+++ VariableAndProperties.playground/Contents.swift
@@ -24,11 +24,47 @@
 //myProperty = 6
 
 
-var myFontSize = 10 {
-    willSet(newFontSize){
-        print("Will set font to \(newFontSize), it was \(myFontSize)")
-    }
-    didSet(oldFontSize){
-        print("Did set font to \(myFontSize), it was \(oldFontSize)")
-    }
-}
+//var myFontSize = 10 {
+//    willSet(newFontSize){
+//        print("Will set font to \(newFontSize), it was \(myFontSize)")
+//    }
+//    didSet(oldFontSize){
+//        print("Did set font to \(myFontSize), it was \(oldFontSize)")
+//    }
+//}
+
+
+// Основы свойств
+
+//class Dog {
+//    var name : String = ""
+//}
+//
+//var myDog = Dog()
+//print(myDog.name)
+//
+//myDog.name = "Djack"
+//print(myDog.name)
+//
+//myDog.name = "Red"
+//print(myDog.name)
+
+
+//class Family {
+//    let dad = "Maksim"
+//    let mother = "Larisa"
+//    let firstSon = "Andrey"
+//    let secondSon = "Bogdan"
+//}
+//
+//
+//var myFamily = Family()
+//print("Моего папу зовут \(myFamily.dad)")
+//print("А нашу маму зовут \(myFamily.mother)")
+//
+//print("Имя нашего первого сына \(myFamily.firstSon)")
+//print("А имя второго сына \(myFamily.secondSon)")
+//
+//print("В нашей семье четыре человека и вот их имена, это папа \(myFamily.dad) это мама \(myFamily.mother) и это наши сыны \(myFamily.firstSon) и \(myFamily.secondSon), мы очень рады что вы познакомились с нами")
+
+
